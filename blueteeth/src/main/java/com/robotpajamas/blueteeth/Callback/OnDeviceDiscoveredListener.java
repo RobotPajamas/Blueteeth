a634467f17ccb00b4766238bc0a4172c2b6074da
SJ <suresh@vicarasolutions.com>
2016-04-03 21:32:59 -0400

    Updated models to support new advertising structure.

---
+++ blueteeth/src/main/java/com/robotpajamas/blueteeth/Callback/OnDeviceDiscoveredListener.java
@@ -0,0 +1,7 @@
+package com.robotpajamas.blueteeth.Callback;
+
+import com.robotpajamas.blueteeth.BlueteethDevice;
+
+public interface OnDeviceDiscoveredListener {
+    void call(BlueteethDevice blueteethDevice);
+}
