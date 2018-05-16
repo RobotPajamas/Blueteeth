481cf31a082948865fc419a456521bd48a874e10
SJ <suresh@vicarasolutions.com>
2018-05-16 18:08:56 -0400

    See #32: Created a Dispatcher class to support executing queue items and timeouts

---
+++ blueteeth/src/main/kotlin/com/robotpajamas/blueteeth/extensions/BluetoothGattCharacteristic.kt
@@ -0,0 +1,6 @@
+package com.robotpajamas.blueteeth.extensions
+
+import android.bluetooth.BluetoothGattCharacteristic
+
+internal inline val BluetoothGattCharacteristic.compositeId: String
+    get() = service.uuid.toString() + toString()
---
+++ blueteeth/src/main/kotlin/com/robotpajamas/blueteeth/internal/Dispatcher.kt
@@ -0,0 +1,60 @@
+package com.robotpajamas.blueteeth.internal
+
+import android.os.Handler
+import android.os.Looper
+import com.robotpajamas.blueteeth.models.QueueItem
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.Executor
+
+internal class Dispatcher {
+
+    // Should be a Queueable?
+    private var active: QueueItem<*>? = null
+    private val queue: Queue<QueueItem<*>> = ConcurrentLinkedQueue()
+    private val dispatchHandler = Handler(Looper.getMainLooper())
+
+    private val executor: Executor = object : Executor {
+        private val handler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            handler.post(command)
+        }
+    }
+
+    @Synchronized
+    fun enqueue(item: QueueItem<*>) {
+        // Append a completion callback
+        item.add { dispatchNext() }
+        queue.add(item)
+        if (active == null) {
+            dispatchNext()
+        }
+    }
+
+    @Synchronized
+    private fun dispatchNext() {
+        // Remove pending timeout runnables
+        dispatchHandler.removeCallbacksAndMessages(null)
+
+        active = queue.poll()
+        active?.let {
+            // Start timeout clock
+            val cancel = Runnable {
+                // TODO: Check if this is doing what I think it is
+                // Capture this current queueItem, and compare it against the active item in X seconds
+                if (it.name != active?.name) {
+                    return@Runnable
+                }
+                active?.timeout()
+            }
+            dispatchHandler.postDelayed(cancel, it.timeout * 1L)
+            executor.execute(it)
+        }
+    }
+
+    @Synchronized
+    internal fun <T> dispatched(name: String): QueueItem<T>? {
+        @Suppress("UNCHECKED_CAST")
+        return if (name == active?.name) active as? QueueItem<T> else null
+    }
+}
